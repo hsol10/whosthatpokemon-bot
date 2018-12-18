663e4d5e96b70adef7ebaaf9e691997597f0810f
N.Smith19 <43012163+NSmith019@users.noreply.github.com>
2018-12-18 14:20:14 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from keep_alive import keep_alive
 import discord
 import os
 
@@ -11,7 +12,9 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author != client.user:
-        await client.send_message(message.channel, message.content[::-1])
+        with open('250px-001Bulbasaur.png', 'rb') as f:
+            await client.send_file(client.get_channel, f)
 
+keep_alive()
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
