108c41651439466367165ddcfea7b848adf6aa1b
hsol10 <43075813+hsol10@users.noreply.github.com>
2019-01-08 12:15:28 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from keep_alive import keep_alive
 import discord
 import os
+import time
+from pokemon import pokemon
+import random
 
 client = discord.Client()
 
@@ -11,9 +14,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author != client.user:
-        with open('250px-001Bulbasaur.png', 'rb') as f:
-            await client.send_file(client.get_channel, f)
+    if message.author != client.user and message.content.startswith('$wtp'):
+        # max_time = 5
+        # start_time = time.time()
+        # while (time.time() - start_time) < max_time:
+        p = pokemon.(pokemon.keys)
+        print(pokemon)
+        await client.send_file(client.get_channel('512765323116806158'), 'bulbasaur.png')
+        await client.wait_for_message(timeout=10, channel=client.get_channel('512765323116806158'), content='bulbasaur')
+        await client.send_message(message.channel, 'done')
+        
 
 keep_alive()
 token = os.environ.get("DISCORD_BOT_SECRET")
