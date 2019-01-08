82c5da3debd1677ec7dc96162c2d0941279c04a3
hsol10 <43075813+hsol10@users.noreply.github.com>
2019-01-08 12:20:11 -0500

    Create pokemon.py

---
+++ pokemon.py
@@ -0,0 +1,10 @@
+pokemon = {
+    'bulbasaur.png': 'bulbasaur',
+    'ivysaur.png': 'ivysaur',
+    'venusaur.png': 'venusaur',
+    'charmander.png': 'charmander',
+    'charmeleon.png': 'charmeleon',
+    'charizard.png': 'charizard',
+    'squirtle.png': 'squirtle',
+    
+}
