2c66d0a7b60bb7752f62d1a199ae6579b615e946
N.Smith19 <43012163+NSmith019@users.noreply.github.com>
2019-01-17 14:13:49 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from keep_alive import keep_alive
 import discord
 import os
+import time
+from pokemon import pokemon
+import random
 
 client = discord.Client()
 
@@ -11,9 +14,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author != client.user:
-        with open('250px-001Bulbasaur.png', 'rb') as f:
-            await client.send_file(client.get_channel, f)
+    if message.author != client.user and message.content.startswith('$wtp'):
+        p = random.choice(list(pokemon.keys()))
+        print(p)
+        pname = pokemon.get(p)
+        await client.send_file(client.get_channel('512765323116806158'), 'pokemon/' + p)
+        timeout_end = time.time() + 10
+        while time.time() < timeout_end:
+            answer = await client.wait_for_message(channel=client.get_channel('512765323116806158'))
+            print(answer.content)
+            if answer.content != '':
+                break
+        if answer.content == pname:
+            await client.send_message(client.get_channel('512765323116806158'), 'correct, good job!')
+            
+        elif answer.content != pname:
+            await client.send_message(client.get_channel('512765323116806158'), 'incorrect')
+        
+        await client.send_message(message.channel, 'done')
+        
 
 keep_alive()
 token = os.environ.get("DISCORD_BOT_SECRET")
